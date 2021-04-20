9320877fa1cf4bb4fde7ffff618a032a5db9e540
orlovkn <orlovkn.job@gmail.com>
2021-04-20 17:34:46 +0300

    add container

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.udemy.com/course/symfony-api-platform-reactjs-full-stack-masterclass/learn/lecture/12288218?start=0#questions
--- index.php
+++ index.php
@@ -3,14 +3,35 @@ declare(strict_types=1);
 
 require __DIR__.'/vendor/autoload.php';
 
-$data = [
-    'one' => 'uno',
-    'two' => 'dois'
-];
-//$json = new \App\Format\JSON();
-$xml = new \App\Format\XML();
+use \App\Controller\IndexController;
+use \App\Service\Serializer;
+use \App\Container;
+use \App\Format\JSON;
+use \App\Format\XML;
 
-$serializer = new \App\Serializer($xml);
-$serializeData= $serializer->serialize($data);
+print_r("Simple Service Container\n\n");
 
-print_r($serializeData);
+$container = new Container();
+
+$container->addService('format.json', function () use ($container) {
+    return new JSON();
+});
+
+$container->addService('format.xml', function () use ($container) {
+    return new XML();
+});
+
+$container->addService('format', function () use ($container) {
+    return $container->getService('format.json');
+});
+
+$container->addService('serializer', function () use ($container) {
+    return new Serializer($container->getService('format'));
+});
+
+$container->addService('controller.index', function () use ($container) {
+    return new IndexController($container->getService('serializer'));
+});
+
+var_dump($container->getServices());
+var_dump($container->getService('controller.index')->index());
